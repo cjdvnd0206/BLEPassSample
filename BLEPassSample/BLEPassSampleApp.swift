d00fc1743c4be7b089a6a0a614360cacdf08bb0a
YOON BYOUNG JIN <cjdvndaudnjf@naver.com>
2022-07-13 17:00:08 +0900

    Initial Commit

---
+++ BLEPassSample/BLEPassSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEPassSampleApp.swift
+//  BLEPassSample
+//
+//  Created by user on 2022/07/13.
+//
+
+import SwiftUI
+
+@main
+struct BLEPassSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BLEPassSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BLEPassSample
+//
+//  Created by user on 2022/07/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
