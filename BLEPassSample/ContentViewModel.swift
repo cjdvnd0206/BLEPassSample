5388351f9ac4af2876149b304e8b0d60117ec9a6
YOON BYOUNG JIN <cjdvndaudnjf@naver.com>
2022-07-19 16:30:21 +0900

    - 문열기 retry 로직 추가

--- BLEPassSample/ContentViewModel.swift
+++ BLEPassSample/ContentViewModel.swift
@@ -9,15 +9,47 @@ import Foundation
 import CoreBluetooth
 
 final class ContentViewModel: NSObject, ObservableObject {
-    var centralManager: CBCentralManager!
+    var centralManager = CBCentralManager()
     var discoveredPeripheral: CBPeripheral?
-    var transferCharacteristic: CBCharacteristic?
     private var characteristic: CBCharacteristic?
     var connectedPeripheral : CBPeripheral?
     let serviceUUID = CBUUID(string: "19B10000-E8F2-537E-4F6C-D104768A1214")
     let characteristicUUID = CBUUID(string: "19B10001-E8F2-537E-4F6C-D104768A1214")
     private var writeType: CBCharacteristicWriteType = .withoutResponse
     @Published var id = ""
+    @Published var buttonText = "문열림 On"
+    @Published var isSending = false {
+        didSet {
+            if isSending {
+                buttonText = "문열림 Off"
+                startScan()
+            } else {
+                buttonText = "문열림 On"
+                stopScan()
+            }
+        }
+    }
+    
+    func buttonToggle() {
+        isSending.toggle()
+    }
+    
+    func startScan() {
+        print("Scan Start")
+        if let peripheral = discoveredPeripheral {
+            centralManager.connect(peripheral, options: nil)
+        } else {
+            centralManager.scanForPeripherals(withServices: [serviceUUID], options: [CBCentralManagerScanOptionAllowDuplicatesKey: true])
+        }
+    }
+    
+    func stopScan() {
+        print("Scan Stopped")
+        centralManager.stopScan()
+        if !isSending, let peripheral = discoveredPeripheral {
+            centralManager.cancelPeripheralConnection(peripheral)
+        }
+    }
     
     func setupCentralManager() {
         centralManager = CBCentralManager(delegate: self, queue: nil, options: [CBCentralManagerOptionShowPowerAlertKey: true])
@@ -25,7 +57,10 @@ final class ContentViewModel: NSObject, ObservableObject {
     
     func sendMessageToDevice(_ message: String) {
         if let data = message.data(using: .utf8), let characteristic = characteristic {
+            print("Send message: \(message)")
             connectedPeripheral?.writeValue(data, for: characteristic, type: writeType)
+        } else {
+            print("characteristic is Empty")
         }
     }
 }
@@ -36,10 +71,10 @@ extension ContentViewModel: CBCentralManagerDelegate {
         case .poweredOn:
             // ... so start working with the peripheral
             print("CBManager is powered on")
-            centralManager.scanForPeripherals(withServices: [serviceUUID], options: [CBCentralManagerScanOptionAllowDuplicatesKey: true])
         case .poweredOff:
             print("CBManager is not powered on")
             // In a real app, you'd deal with all the states accordingly
+            stopScan()
             return
         case .resetting:
             print("CBManager is resetting")
@@ -93,7 +128,7 @@ extension ContentViewModel: CBCentralManagerDelegate {
         print("Peripheral Connected")
         
         // 스캔을 중단한다
-        centralManager.stopScan()
+        stopScan()
         connectedPeripheral = peripheral
         // 검색 콜백을 받기위한 델리게이트 선언
         peripheral.delegate = self
@@ -111,10 +146,14 @@ extension ContentViewModel: CBPeripheralDelegate {
         
         // 원하는 특성을 검색한다
         // 둘 이상의 서비스를 대비해 배열에 넣고 순환하여 탐색
-        guard let peripheralServices = peripheral.services else { return }
+        guard let peripheralServices = peripheral.services else {
+            print("peripheralServices Error...")
+            return
+        }
         for service in peripheralServices {
             peripheral.discoverCharacteristics([characteristicUUID], for: service)
         }
+        print("Discover Success...")
     }
     
     func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
@@ -125,16 +164,32 @@ extension ContentViewModel: CBPeripheralDelegate {
         }
         
         // 만약을 대비해 배열을 다시 순환시켜 특성이 맞는지 체크한다
-        guard let serviceCharacteristics = service.characteristics else { return }
+        guard let serviceCharacteristics = service.characteristics else {
+            print("serviceCharacteristics Error...")
+            return
+        }
+        
         for characteristic in serviceCharacteristics where characteristic.uuid == characteristicUUID {
             // 구독한다
-            print("Subscribe Success...")
             peripheral.setNotifyValue(true, for: characteristic)
             self.characteristic = characteristic
             writeType = characteristic.properties.contains(.write) ? .withResponse :  .withoutResponse
         }
-        
+        print("Subscribe Success...")
         sendMessageToDevice(id)
         // 여기까지 완료되었으면 데이터를 받는걸 기다린다
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didWriteValueFor characteristic: CBCharacteristic, error: Error?) {
+        if let error = error {
+            print("Error didWriteValueFor: \(error.localizedDescription)")
+            return
+        }
+        print("WriteValue Success...")
+        if isSending {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
+                self.centralManager.connect(peripheral, options: nil)
+            })
+        }
     }
 }
